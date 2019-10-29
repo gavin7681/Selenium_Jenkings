d18154be31e534fda6174b050ddc1917a31c4fbc
gavin7681 <pr.govind81@gmail.com>
2019-10-29 15:08:13 -0500

    SYSO Removed

--- src/main/java/Jenkins/Test/testNG_OpenBrowser.java
+++ src/main/java/Jenkins/Test/testNG_OpenBrowser.java
@@ -24,9 +24,7 @@ public class testNG_OpenBrowser {
 		public void testEasy() {	
 			driver.get("http://demo.guru99.com/test/guru99home/");  
 			String title = driver.getTitle();	
-			System.out.println("Verifying Title");
-			System.out.println("Test Started");
-			Assert.assertTrue(title.contains("Demo Guru99 Page"));
+				Assert.assertTrue(title.contains("Demo Guru99 Page"));
 			System.out.println("Test Success");
 			
 		}	
