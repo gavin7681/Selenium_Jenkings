0c748107fbfc961ebf14e97900440bed6a5cfd57
gavin7681 <pr.govind81@gmail.com>
2022-09-20 19:16:34 -0400

    commit

--- src/main/java/Jenkins/Test/testNG_OpenBrowser.java
+++ src/main/java/Jenkins/Test/testNG_OpenBrowser.java
@@ -9,7 +9,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class testNG_OpenBrowser {
-  
+  //Open Browser Test Cases
 	 private WebDriver driver;
 		@BeforeTest
 		public void beforeTest() {	
